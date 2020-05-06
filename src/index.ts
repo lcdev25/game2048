d1377508dfa230a7c6f58aeb6f2a86c937bea639
lchandna <lchandna@gainsight.com>
2020-05-06 21:24:28 +0530

    Adding server

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import path from 'path';
+
+const app = express();
+
+let counter = 0;
+app.use(express.json());
+
+// if(process.env.NODE_END === 'production') {
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    console.log(`Starting game: ${counter++}`);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
