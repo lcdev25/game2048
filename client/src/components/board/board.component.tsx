d689fdff9529ad63ebaf10ebdc18dfc025d2a2a8
lchandna <lchandna@gainsight.com>
2020-05-06 22:54:52 +0530

    Removing swipe library and adding custom swipe detection logic

--- client/src/components/board/board.component.tsx
+++ client/src/components/board/board.component.tsx
@@ -1,8 +1,6 @@
 import { connect, ConnectedProps } from 'react-redux';
 import { AppState } from '../../store/app.types';
 import React, { Fragment, useEffect } from 'react';
-// @ts-ignore
-import Swipe from 'react-easy-swipe';
 
 import {
     moveBottom,
@@ -34,6 +32,10 @@ const getCellLabel = (cell: number | null) => {
 
 const Board = (props: Props) => {
     const cells = props.boardState.present.cells;
+    let touchStartX: number;
+    let touchStartY: number;
+    let touchEndX: number;
+    let touchEndY: number;
 
     useEffect(() => {
         document.onkeydown = checkKey;
@@ -42,40 +44,79 @@ const Board = (props: Props) => {
         };
     }, []);
 
+    function touchStart(e: any) {
+        touchStartX = e.touches[0].clientX;
+        touchStartY = e.touches[0].clientY;
+    }
+
+    function touchMove(e: any) {
+        touchEndX = e.touches[0].clientX;
+        touchEndY = e.touches[0].clientY;
+    }
+
+    function touchEnd(e: any) {
+        console.log(touchStartX, touchStartY);
+        console.log(touchEndX, touchEndY);
+        const horDis = Math.abs(touchEndX - touchStartX);
+        const verDis = Math.abs(touchEndY - touchStartY);
+        if (horDis > 50 || verDis > 50) {
+            if (horDis > verDis) {
+                //Right or left move
+                if (touchEndX - touchStartX > 0) {
+                    //right swipe
+                    onMoveRight(e);
+                } else {
+                    //left swipe
+                    onMoveLeft(e);
+                }
+            } else {
+                //Up or down move
+                if (touchEndY - touchStartY > 0) {
+                    //Down swipe
+                    onMoveBottom(e);
+                } else {
+                    //Up swipe
+                    onMoveTop(e);
+                }
+            }
+        }
+        console.log(horDis, verDis);
+    }
+
     function checkKey(e: any) {
         e = e || window.event;
 
         if (e.keyCode == '38') {
-            if (gameOver()) return;
-            props.moveTop();
+            onMoveTop(e);
         } else if (e.keyCode == '40') {
-            if (gameOver()) return;
-            props.moveBottom();
+            onMoveBottom(e);
         } else if (e.keyCode == '37') {
-            if (gameOver()) return;
-            props.moveLeft();
+            onMoveLeft(e);
         } else if (e.keyCode == '39') {
-            if (gameOver()) return;
-            props.moveRight();
+            onMoveRight(e);
         }
     }
 
-    const onMoveLeft = () => {
+    const onMoveLeft = (e: any) => {
+        e.preventDefault();
         if (gameOver()) return;
         props.moveLeft();
     };
 
-    const onMoveRight = () => {
+    const onMoveRight = (e: any) => {
+        e.preventDefault();
         if (gameOver()) return;
         props.moveRight();
     };
 
-    const onMoveTop = () => {
+    const onMoveTop = (e: any) => {
+        e.preventDefault();
         if (gameOver()) return;
         props.moveTop();
     };
 
-    const onMoveBottom = () => {
+    const onMoveBottom = (e: any) => {
+        e.preventDefault();
         if (gameOver()) return;
         props.moveBottom();
     };
@@ -86,127 +127,125 @@ const Board = (props: Props) => {
 
     return (
         <Fragment>
-            <Swipe
-                onSwipeLeft={onMoveLeft}
-                onSwipeRight={onMoveRight}
-                onSwipeUp={onMoveTop}
-                onSwipeDown={onMoveBottom}
+            <div
+                className="board-container"
+                onTouchStart={(e) => touchStart(e)}
+                onTouchMove={(e) => touchMove(e)}
+                onTouchEnd={(e) => touchEnd(e)}
             >
-                <div className="board-container">
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[0][0].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[0][0].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[0][1].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[0][1].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[0][2].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[0][2].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[0][3].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[0][3].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[1][0].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[1][0].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[1][1].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[1][1].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[1][2].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[1][2].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[1][3].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[1][3].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[2][0].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[2][0].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[2][1].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[2][1].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[2][2].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[2][2].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[2][3].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[2][3].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[3][0].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[3][0].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[3][1].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[3][1].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[3][2].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[3][2].value)}
-                    </div>
-                    <div
-                        className={`board-cell board-cell-${
-                            getCellLabel(cells[3][3].value) || `null`
-                        }`}
-                    >
-                        {getCellLabel(cells[3][3].value)}
-                    </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[0][0].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[0][0].value)}
                 </div>
-            </Swipe>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[0][1].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[0][1].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[0][2].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[0][2].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[0][3].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[0][3].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[1][0].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[1][0].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[1][1].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[1][1].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[1][2].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[1][2].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[1][3].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[1][3].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[2][0].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[2][0].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[2][1].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[2][1].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[2][2].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[2][2].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[2][3].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[2][3].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[3][0].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[3][0].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[3][1].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[3][1].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[3][2].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[3][2].value)}
+                </div>
+                <div
+                    className={`board-cell board-cell-${
+                        getCellLabel(cells[3][3].value) || `null`
+                    }`}
+                >
+                    {getCellLabel(cells[3][3].value)}
+                </div>
+            </div>
         </Fragment>
     );
 };
--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ const app = express();
 let counter = 0;
 app.use(express.json());
 
-// if(process.env.NODE_END === 'production') {
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-    console.log(`Starting game: ${counter++}`);
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        console.log(`Starting game: ${counter++}`);
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
