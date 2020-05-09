835a0fbf8cbb97f1b8cae1c02e6eeccfcffe5f92
lchandna <lchandna@gainsight.com>
2020-05-09 18:58:49 +0530

    Closes #3 Since we were updating the timesNewGame, timesUndo, timesRedo directly from the data received from the API call, this data was getting reset for every new gaming session as this data was also resetting on the client. Added the fix now to increment the counts with the counts received from the user.

--- src/services/game-info.ts
+++ src/services/game-info.ts
@@ -1,14 +1,14 @@
-import GameModel, { GameInfo } from '../models/GameInfo';
+import GameInfoModel, { GameInfo } from '../models/GameInfo';
 
 export async function startGame(gameId, ipInfo) {
     let gameInfo: GameInfo;
     if (gameId) {
-        gameInfo = await GameModel.findById(gameId);
+        gameInfo = await GameInfoModel.findById(gameId);
     }
     if (!gameInfo) {
-        gameInfo = new GameModel();
+        gameInfo = new GameInfoModel();
     } else {
-        gameInfo.timesNewGame = gameInfo.timesNewGame + 1;
+        gameInfo.timesNewGame += 1;
     }
     gameInfo.ipInfo = ipInfo || {};
     return gameInfo.save();
@@ -16,13 +16,13 @@ export async function startGame(gameId, ipInfo) {
 
 export async function getGlobalStats() {
     const globalGamesPlayed = (
-        await GameModel.aggregate([
+        await GameInfoModel.aggregate([
             { $group: { _id: null, sum: { $sum: '$timesNewGame' } } },
         ])
     )[0]['sum'];
     const fourHrAgo = new Date();
     fourHrAgo.setMinutes(fourHrAgo.getMinutes() - 240);
-    const globalPlayersPlaying = await GameModel.count({
+    const globalPlayersPlaying = await GameInfoModel.count({
         updatedAt: { $gt: fourHrAgo },
     });
     return {
@@ -32,5 +32,40 @@ export async function getGlobalStats() {
 }
 
 export async function updateGameInfo(gameId: string, data: GameInfo) {
-    await GameModel.findByIdAndUpdate(gameId, { $set: data });
+    const gameInfo: GameInfo = await GameInfoModel.findById(gameId);
+    if (gameInfo) {
+        for (let prop of Object.keys(data)) {
+            switch (prop) {
+                case 'timesUndo':
+                    gameInfo.timesUndo += data.timesUndo;
+                    break;
+                case 'timesRedo':
+                    gameInfo.timesRedo += data.timesRedo;
+                    break;
+                case 'timesNewGame':
+                    gameInfo.timesNewGame += 1;
+                    break;
+                case 'hasVisitedProfile':
+                    if (!gameInfo.hasVisitedProfile) {
+                        gameInfo.hasVisitedProfile = data.hasVisitedProfile;
+                    }
+                    break;
+                case 'hasVisitedContribute':
+                    if (!gameInfo.hasVisitedContribute) {
+                        gameInfo.hasVisitedContribute =
+                            data.hasVisitedContribute;
+                    }
+                    break;
+                case 'hasVisitedOriginal':
+                    if (!gameInfo.hasVisitedOriginal) {
+                        gameInfo.hasVisitedOriginal = data.hasVisitedOriginal;
+                    }
+                    break;
+                default:
+                    console.log(`Invalid property found for update: ${prop}`);
+                    break;
+            }
+            await gameInfo.save();
+        }
+    }
 }
